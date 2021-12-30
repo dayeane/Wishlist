394bdb6bd7fae467e8872a5c817c2e3b4174091e
Dayeane <dayeane85@gmail.com>
2021-12-30 10:06:21 -0800

    seeds updated for more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(name: "jane", email_address: "jane@example.com",password: "1234567")
 
-item = Item.create(name: "lamp", description:"shine very iluminate" , image_url: Faker::LoremFlickr.image(size: “320x240” ,search_terms: ['sports', 'fitness']))
+p "creating Items"
+(0..20).each do
+  Item.create(name: Faker::Movies::StarWars.character, description: Faker::Movies::StarWars.quote, image_url: Faker::LoremFlickr.image(size: "320x240" , search_terms: ['Starwars']))
+end
 
-user.lists.create(name: "home")
+["Nicholas", "Ryan", "Dayeane"].each do |name|
+  p "creating User #{name}"
+  user = User.create(name: name, email_address: "#{name}@example.com", password: "1234567")
 
-list.items << item
+  p "Creating user #{name} List"
+  list = user.lists.create(name: "User #{name} List")
+
+  p "Adding Items to #{name} List"
+  Item.all.each do |item|
+    p "adding #{item.name} to user #{name} list"
+    list.items << item
+  end
+end
+
+p "Finish creating Seeds"
