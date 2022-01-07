46779a90a431bc57bd4a6444738af9946edc4583
Ryan Kotsovolos <rkotsovolos@gmail.com>
2022-01-07 09:26:24 -0800

    Home page description of app

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -2,8 +2,20 @@
 
 function HomePage(){
     return(
-        <div>   
-           <h4 className="mt-4 mr-2" >Welcome To the WishList!</h4> 
+        <div className="container">   
+            <div className="row justify-content-center">
+                <h4 className="mt-4 mr-2" >Welcome To the WishList!</h4> 
+                <p>Our mission: This app is here to provide a service, and that service is to help our users make a list, but not just any kind of list.
+                    We make it easy for users to create their own personal wishlists with a customizable name, allowing you to add items to those list with just a simple 
+                    click. Don't see what you're looking for? No biggie, we allow you to add your own items that you found from outside sources, with the name of the product, price, image, and description.
+                    This will allow you to keep track of multiple items from outside sources if you so wish to do so.
+                </p>
+                <br></br>
+                <p>We pride ourselves with how easy it is to get started on creating, adding, updating and deleting products and lists with just a few key strokes. The user should be able to login 
+                    with ease and be confident in how to navigate and use the Wishlist application. 
+                </p>
+                <h3>Let's go shopping!</h3>
+            </div>
         </div>
     )
 }
